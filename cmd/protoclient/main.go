8307cfd64d9639fd0462c577e63d512874914106
Green Lightning <GreenLightning.git@googlemail.com>
2018-11-29 17:13:38 +0100

    Add protoclient command

---
+++ cmd/protoclient/main.go
@@ -0,0 +1,12 @@
+// protoclient is a command line tool used to encrypt / decrypt and send / receive messages using the communication protocol.
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/iot-bp-project-2018/raspi-server/internal/commproto"
+)
+
+func main() {
+	fmt.Println("Hello, Sailor!")
+}
